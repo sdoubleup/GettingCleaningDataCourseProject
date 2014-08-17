adc3c06856399c629fa221ddf5a8b547af59a679
Sam Pennington <sdoubleup@users.noreply.github.com>
2014-08-17 20:34:13 +0100

    Tabs to spaces

--- run_analysis.R
+++ run_analysis.R
@@ -65,150 +65,150 @@ PRETTY_PRINT <- FALSE
 ## Checks whether the data folder exists in the current working directory.
 ## Stops with an error message if it doesnt.
 checkDirectory <- function() {
-	if (!file.exists(DATA_FOLDER)) {
-		stop(paste("Missing directory:", DATA_FOLDER, "; This is required to proceed"))
-	}
-	return(TRUE)
+    if (!file.exists(DATA_FOLDER)) {
+        stop(paste("Missing directory:", DATA_FOLDER, "; This is required to proceed"))
+    }
+    return(TRUE)
 }
 
 ## If the data folder exists we switch to that as the current working directory.
 changeWorkingDirectoryToDataFolder <- function() {
-	if (checkDirectory()) {
-		currentWD <- getwd()
-		print(paste("Current working directory:", currentWD))
-		setwd(DATA_FOLDER)
-		print(paste("Working directory changed to:", getwd()))
-	}
+    if (checkDirectory()) {
+        currentWD <- getwd()
+        print(paste("Current working directory:", currentWD))
+        setwd(DATA_FOLDER)
+        print(paste("Working directory changed to:", getwd()))
+    }
 }
 
 ## Check if the expected files that we will be cleaning exist in the current working directory.
 ## If they dont we need to error out.
 checkFiles <- function() {
 
-	filenames <- c(SUBJECT_TRAIN_FILE, 
-				   X_TRAIN_FILE, 
-				   Y_TRAIN_FILE, 
-				   SUBJECT_TEST_FILE,
-				   X_TEST_FILE,
-				   Y_TEST_FILE,
-				   FEATURES_FILE, 
-				   ACTIVITY_LABELS_FILE)
+    filenames <- c(SUBJECT_TRAIN_FILE, 
+                   X_TRAIN_FILE, 
+                   Y_TRAIN_FILE, 
+                   SUBJECT_TEST_FILE,
+                   X_TEST_FILE,
+                   Y_TEST_FILE,
+                   FEATURES_FILE, 
+                   ACTIVITY_LABELS_FILE)
 
-	for (i in seq_along(filenames)) {
-		if (!file.exists(filenames[i])) {
-			stop(paste("Missing file: ", filenames[i], "; Is your working directory correct?"))
-		}
-	}
+    for (i in seq_along(filenames)) {
+        if (!file.exists(filenames[i])) {
+            stop(paste("Missing file: ", filenames[i], "; Is your working directory correct?"))
+        }
+    }
 
-	print("All required files exist.")
+    print("All required files exist.")
 }
 
 getFeaturesTable <- function() {
-	return (read.table(FEATURES_FILE, header = FALSE))
+    return (read.table(FEATURES_FILE, header = FALSE))
 }
 
 getActivityTable <- function() {
-	activityTable = read.table(ACTIVITY_LABELS_FILE, header = FALSE)
-	colnames(activityTable) = c(ACTIVITY_ID_COL, ACTIVITY_TYPE_COL)
-	return (activityTable)
+    activityTable = read.table(ACTIVITY_LABELS_FILE, header = FALSE)
+    colnames(activityTable) = c(ACTIVITY_ID_COL, ACTIVITY_TYPE_COL)
+    return (activityTable)
 }
 
 mergeTrainingFiles <- function(featuresTable, activityTable) {
-	## Load the tables that we will be combining.
-	subjectTrainTable = read.table(SUBJECT_TRAIN_FILE, header = FALSE)
-	xTrainTable = read.table(X_TRAIN_FILE, header = FALSE)
-	yTrainTable = read.table(Y_TRAIN_FILE, header = FALSE)
-	## Assign column names.
-	colnames(subjectTrainTable) = SUBJECT_ID_COL
-	colnames(xTrainTable) = featuresTable[, 2]
-	colnames(yTrainTable) = ACTIVITY_ID_COL
-	return (cbind(yTrainTable, subjectTrainTable, xTrainTable))
+    ## Load the tables that we will be combining.
+    subjectTrainTable = read.table(SUBJECT_TRAIN_FILE, header = FALSE)
+    xTrainTable = read.table(X_TRAIN_FILE, header = FALSE)
+    yTrainTable = read.table(Y_TRAIN_FILE, header = FALSE)
+    ## Assign column names.
+    colnames(subjectTrainTable) = SUBJECT_ID_COL
+    colnames(xTrainTable) = featuresTable[, 2]
+    colnames(yTrainTable) = ACTIVITY_ID_COL
+    return (cbind(yTrainTable, subjectTrainTable, xTrainTable))
 }
 
 mergeTestingFiles <- function(featuresTable, activityTable) {
-	## Load the tables that we will be combining.
-	subjectTestTable = read.table(SUBJECT_TEST_FILE, header = FALSE)
-	xTestTable = read.table(X_TEST_FILE, header = FALSE)
-	yTestTable = read.table(Y_TEST_FILE, header = FALSE)
-	## Assign names.
-	colnames(subjectTestTable) = SUBJECT_ID_COL
-	colnames(xTestTable) = featuresTable[,2]
-	colnames(yTestTable) = ACTIVITY_ID_COL
-	return (cbind(yTestTable, subjectTestTable, xTestTable))
+    ## Load the tables that we will be combining.
+    subjectTestTable = read.table(SUBJECT_TEST_FILE, header = FALSE)
+    xTestTable = read.table(X_TEST_FILE, header = FALSE)
+    yTestTable = read.table(Y_TEST_FILE, header = FALSE)
+    ## Assign names.
+    colnames(subjectTestTable) = SUBJECT_ID_COL
+    colnames(xTestTable) = featuresTable[,2]
+    colnames(yTestTable) = ACTIVITY_ID_COL
+    return (cbind(yTestTable, subjectTestTable, xTestTable))
 }
 
 ## Builds a mask to enable the filtering of the merged data so that
 ## it only contains the columns we are interested in.
 getColumnNameMask <- function() {
-	## Create a logicalVector that contains TRUE values columns with names
-	## that equate to the mean and std columns.
-	## Because the columns can have some random text before and after
-	## the tid-bit of the name we are after we are doing a grep.
-	## The OR'd groups are our individual columns, the AND terms in each group
-	## are where we are specifying our text search while eliminating some false matches.
-	return ((grepl(ACTIVITY_ID_COL, finalDataColumnNames) |
-	         grepl(SUBJECT_ID_COL, finalDataColumnNames) |
-			 (grepl("-mean..", finalDataColumnNames) & !grepl("-meanFreq..", finalDataColumnNames) & !grepl("mean..-", finalDataColumnNames)) | 
-			 (grepl("-std..", finalDataColumnNames) & !grepl("-std()..-", finalDataColumnNames))))
+    ## Create a logicalVector that contains TRUE values columns with names
+    ## that equate to the mean and std columns.
+    ## Because the columns can have some random text before and after
+    ## the tid-bit of the name we are after we are doing a grep.
+    ## The OR'd groups are our individual columns, the AND terms in each group
+    ## are where we are specifying our text search while eliminating some false matches.
+    return ((grepl(ACTIVITY_ID_COL, finalDataColumnNames) |
+             grepl(SUBJECT_ID_COL, finalDataColumnNames) |
+             (grepl("-mean..", finalDataColumnNames) & !grepl("-meanFreq..", finalDataColumnNames) & !grepl("mean..-", finalDataColumnNames)) | 
+             (grepl("-std..", finalDataColumnNames) & !grepl("-std()..-", finalDataColumnNames))))
 }
 
 cleanupColumnNames <- function(columnNames) {
-	## Clean up the names.
-	## Basically standardise some capitalisation and replace some
-	## shorthand naming with some full naming.
-	for (i in 1:length(columnNames)) {
-  		columnNames[i] = gsub("\\()", "", columnNames[i])
-  		columnNames[i] = gsub("-std$", "StandardDeviation", columnNames[i])
-  		columnNames[i] = gsub("-mean", "Mean", columnNames[i])
-  		columnNames[i] = gsub("^(t)", "Time", columnNames[i])
-  		columnNames[i] = gsub("^(f)", "Frequency", columnNames[i])
-  		columnNames[i] = gsub("([Gg]ravity)", "Gravity", columnNames[i])
-  		columnNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)", "Body", columnNames[i])
-  		columnNames[i] = gsub("[Gg]yro", "Gyro", columnNames[i])
-  		columnNames[i] = gsub("AccMag", "AccelerationMagnitude", columnNames[i])
-  		columnNames[i] = gsub("([Bb]odyaccjerkmag)", "BodyAccelerationJerkMagnitude", columnNames[i])
-  		columnNames[i] = gsub("JerkMag", "JerkMagnitude", columnNames[i])
-  		columnNames[i] = gsub("GyroMag", "GyroMagnitude", columnNames[i])
-	}
-	return (columnNames)
+    ## Clean up the names.
+    ## Basically standardise some capitalisation and replace some
+    ## shorthand naming with some full naming.
+    for (i in 1:length(columnNames)) {
+        columnNames[i] = gsub("\\()", "", columnNames[i])
+        columnNames[i] = gsub("-std$", "StandardDeviation", columnNames[i])
+        columnNames[i] = gsub("-mean", "Mean", columnNames[i])
+        columnNames[i] = gsub("^(t)", "Time", columnNames[i])
+        columnNames[i] = gsub("^(f)", "Frequency", columnNames[i])
+        columnNames[i] = gsub("([Gg]ravity)", "Gravity", columnNames[i])
+        columnNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)", "Body", columnNames[i])
+        columnNames[i] = gsub("[Gg]yro", "Gyro", columnNames[i])
+        columnNames[i] = gsub("AccMag", "AccelerationMagnitude", columnNames[i])
+        columnNames[i] = gsub("([Bb]odyaccjerkmag)", "BodyAccelerationJerkMagnitude", columnNames[i])
+        columnNames[i] = gsub("JerkMag", "JerkMagnitude", columnNames[i])
+        columnNames[i] = gsub("GyroMag", "GyroMagnitude", columnNames[i])
+    }
+    return (columnNames)
 }
 
 ## Try to output to the set filepath. If the file aready exists
 ## we back it up and then output.
 ## Note that we only keep one backup at a time.
 outputFile <- function(data) {
-	print (paste("Trying to output to file:", OUTPUT_FILEPATH))
-	if (file.exists(OUTPUT_FILEPATH)) {
-		print("File already exists, backing up and overriding.")
-		if (file.exists(paste(OUTPUT_FILEPATH, ".back"))) {
-			file.remove(paste(OUTPUT_FILEPATH, ".back"))
-		}
-		file.rename(OUTPUT_FILEPATH, paste(OUTPUT_FILEPATH, ".back"))
-		print("Previous file backed up")
-	}
+    print (paste("Trying to output to file:", OUTPUT_FILEPATH))
+    if (file.exists(OUTPUT_FILEPATH)) {
+        print("File already exists, backing up and overriding.")
+        if (file.exists(paste(OUTPUT_FILEPATH, ".back"))) {
+            file.remove(paste(OUTPUT_FILEPATH, ".back"))
+        }
+        file.rename(OUTPUT_FILEPATH, paste(OUTPUT_FILEPATH, ".back"))
+        print("Previous file backed up")
+    }
 
-	if (PRETTY_PRINT) {
-		## temporarily set the width for writing out the file so it
-		## doesnt get wrapped horribly.
-		cachedOption <- getOption("width")
-		options(width = 1500)
-		if ("Hmisc" %in% rownames(installed.packages())) {
-			library(Hmisc)
-			capture.output(print.char.matrix(data,
-						  					 cell.align = "cen",
-						  					 col.txt.align = "cen", 
-                          					 col.names = TRUE, 
-                          					 row.names = FALSE),
-                   		   file = OUTPUT_FILEPATH)
-		} else {
-			capture.output(print(data, print.gap=3), file = OUTPUT_FILEPATH)
-		}
-		options(width = cachedOption)
-	} else {
-		write.table(data, OUTPUT_FILEPATH, row.names=FALSE, sep=',');
-	}
-	
-	print("File written.")
+    if (PRETTY_PRINT) {
+        ## temporarily set the width for writing out the file so it
+        ## doesnt get wrapped horribly.
+        cachedOption <- getOption("width")
+        options(width = 1500)
+        if ("Hmisc" %in% rownames(installed.packages())) {
+            library(Hmisc)
+            capture.output(print.char.matrix(data,
+                                             cell.align = "cen",
+                                             col.txt.align = "cen", 
+                                             col.names = TRUE, 
+                                             row.names = FALSE),
+                           file = OUTPUT_FILEPATH)
+        } else {
+            capture.output(print(data, print.gap=3), file = OUTPUT_FILEPATH)
+        }
+        options(width = cachedOption)
+    } else {
+        write.table(data, OUTPUT_FILEPATH, row.names=FALSE, sep=',');
+    }
+    
+    print("File written.")
 }
 
 ## ----------------------------------------------------------------------------
@@ -270,8 +270,8 @@ absoluteFinalData = filteredFinalData[,names(filteredFinalData) != ACTIVITY_TYPE
 ## Summarise. 
 ## Only include the mean of each variable for each activity and subject.
 tidied = aggregate(absoluteFinalData[,names(absoluteFinalData) != c(ACTIVITY_ID_COL, SUBJECT_ID_COL)],
-				   by = list(activityId = absoluteFinalData$activityId, subjectId = absoluteFinalData$subjectId),
-				   mean)
+                   by = list(activityId = absoluteFinalData$activityId, subjectId = absoluteFinalData$subjectId),
+                   mean)
 ## Merge with the activity type to include descriptive activity names.
 tidied = merge(tidied, activityTable, by = ACTIVITY_ID_COL, all.x = TRUE)
 
